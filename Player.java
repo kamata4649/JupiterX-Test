80e9a50db4337d0c6bbd979723441710823f0421
kamata4649 <kappa.boogie.woogie@gmail.com>
2017-09-25 19:55:18 +0900

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,11 @@
 
+//ゲームモード gamemode
+	public int SURVIVAL = 0;
+	public int CREATIVE = 1;
+	public int ADVENTURE = 2;
+	public int SPECTATOR = 3;
+	public int VIEW = SPECTATOR;
+
 //Skin boolean
 public static boolean isValidSkin(String skin){
 	return skin.length() == 64 * 64 * 4  || skin.length() == 64 * 32 * 4;
